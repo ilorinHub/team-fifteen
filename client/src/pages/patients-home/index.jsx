14576e7bd59bbeda7ad2da193f905469d818c218
khodijah0498 <khodijahlawal@gmail.com>
2023-01-29 14:12:12 +0100

    user dashboard

--- client/src/pages/patients-home/index.jsx
+++ client/src/pages/patients-home/index.jsx
@@ -13,12 +13,54 @@ const PatientHome = () => {
               <h1>Hello Muh_Jumah</h1>
               <span>How are you feeling today</span>
             </div>
-            <Button label="Book Appointment" mxWt="max-w-[9.5rem]"/>
+            <Button label="Book Appointment" mxWt="max-w-[9.5rem]" />
+          </div>
+          <div className="patient-doctor-data">
+            <div className="patient-data-template">
+              <div className="doctor-image">
+                <img src="/img/Frame 6872.webp" alt="Frame 6872" />
+                <span>Your Doctor</span>
+              </div>
+              <div className="doctor-name">
+                <img src="/img/Frame 6874.webp" alt="Frame 6874" />
+                <div className="flex flex-col">
+                  <span>Yusuf Muhammed</span>
+                  <span>Cardiologist</span>
+                </div>
+                <img src="/img/Frame 6877.webp" alt="vector3" />
+                <img src="/img/Frame 6878.webp" alt="vector4" />
+              </div>
+            </div>
+            <div className="patient-data-template">
+              <div className="doctor-image">
+                <img src="/img/Frame 6879.webp" alt="Frame 6879" />
+                <span>Your Data</span>
+              </div>
+              <div className="flex justify-between items-center">
+                <div className="flex flex-col">
+                  <span>Weight</span>
+                  <span>56kg</span>
+                </div>
+                <div className="flex flex-col">
+                  <span>Height</span>
+                  <span>72m</span>
+                </div>
+                <div className="flex flex-col">
+                  <span>Blood Type</span>
+                  <span>A+</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="patient-report">
+            <h2>Reports</h2>
+            <div className="patient-info">
+              
+
+            </div>
           </div>
         </div>
-        <div className="patient-dashboard2">
-          
-        </div>
+        <div className="patient-dashboard2"></div>
       </div>
     </PatientDashboardLayout>
   );
