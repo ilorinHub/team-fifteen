c80d06f2667cd18789337c3d7c00fe375302cf45
khodijah0498 <khodijahlawal@gmail.com>
2023-01-29 11:22:22 +0100

    landing page

--- client/src/pages/patients-home/index.jsx
+++ client/src/pages/patients-home/index.jsx
@@ -1,9 +1,27 @@
+import Button from "../../components/atoms/button";
 import PatientDashboardLayout from "../../templates/patient-dashboard-layout";
 
 import "./PatientHomeStyles.scss";
 
 const PatientHome = () => {
-  return <PatientDashboardLayout>PatientHome</PatientDashboardLayout>;
+  return (
+    <PatientDashboardLayout>
+      <div className="patient-dashboard-layout">
+        <div className="patient-dashboard1">
+          <div className="patient-dashboard-title-stage">
+            <div className="patient-dashboard-title">
+              <h1>Hello Muh_Jumah</h1>
+              <span>How are you feeling today</span>
+            </div>
+            <Button label="Book Appointment" mxWt="max-w-[9.5rem]"/>
+          </div>
+        </div>
+        <div className="patient-dashboard2">
+          
+        </div>
+      </div>
+    </PatientDashboardLayout>
+  );
 };
 
 export default PatientHome;
